1762b0eeea56a8e40ae8155054d6c213ab0e004b
Rosman <rosman.rodriguez.paredez@utelvt.edu.ec>
2025-10-11 15:30:47 -0500

    ACTUALIZACION BACKEND

--- misapis/server/server.js
+++ misapis/server/server.js
@@ -8,19 +8,14 @@ export default class Server {
         this.port = 3000;
         this.miapi = '/api/';
 
-        //Mickevares
         this.middlewares();
-        // Rutas de mi aplicación
         this.routes();
     }
     middlewares() {
-        // CORS
         this.app.use(cors());
 
-        // Lectura y parseo del body 
         this.app.use(express.json());
 
-        // Directorio público 
         this.app.use(express.static('public'));
 
     }
