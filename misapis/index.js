cda310a5f2d4b0cce8760413a0449d39600041e0
Rosman-Jair <Rosman-Jair>
2025-10-29 20:32:31 -0500

    AGREGANDO CAMBIOS EN PUT GET POST DELET

--- misapis/index.js
+++ misapis/index.js
@@ -1,6 +1,8 @@
 import Server from "./server/server.js";
-
+import colors from 'colors';
+import dotenv from 'dotenv';
+dotenv.config();
 const server = new Server();
 server.listen();
 
-console.log("Anuel AA");
+console.log("Anuel AA - La rosman");
